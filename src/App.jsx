15f366a31b141160e1a9983b95c88dd508212b33
melikeozlen <melikeozlen@gmail.com>
2023-11-22 21:36:23 +0300

    Modals - last Layout Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import React from 'react'
-import { RegularList } from './components/Lists/lists/regular'
-import { authors } from './data/authors'
-import LargeListAuthorType from './components/Lists/authors/LargeListType'
-import SmallListAuthorType from './components/Lists/authors/SmallListType'
-import { SmallListBookType } from './components/Lists/books/SmallListType'
-import { LargeListBookType } from './components/Lists/books/LargeListType'
-import { books } from './data/books'
+import { Modal } from './components/Modals/Modals'
 import { NumberedList } from './components/Lists/lists/Numbered'
-
+import SmallListAuthorType from './components/Lists/authors/SmallListType'
+import { authors } from './data/authors'
 const App = () => {
   return (
-    <div>
-
-      <h3> Author List </h3>
-      <NumberedList items={authors} sourceName={"author"} ItemComponent={SmallListAuthorType} />
-      <NumberedList items={authors} sourceName={"author"} ItemComponent={LargeListAuthorType} />
-
-      <h3> Book List</h3>
-
-      <RegularList items={books} sourceName={"book"} ItemComponent={SmallListBookType} />
-      <RegularList items={books} sourceName={"book"} ItemComponent={LargeListBookType} />
-
-    </div>
+    <>
+      <Modal>
+        <NumberedList items={authors} sourceName={"author"} ItemComponent={SmallListAuthorType} />
+      </Modal>
+    </>
   )
 }
 
