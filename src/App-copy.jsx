a715cce91cf1f4b5181214784f6b235d3c8b3abb
melikeozlen <melikeozlen@gmail.com>
2023-11-22 20:29:38 +0300

    last lesson copy

---
+++ src/App-copy.jsx
@@ -0,0 +1,38 @@
+// * -- Layout Components V.1 -- 
+
+// import React from 'react'
+// import SplitPage from './components/splite-page'
+// import { RightComponent } from './components/right-component'
+// import { LeftComponent } from './components/left-component'
+
+// const App = () => {
+//   return (
+//     <>
+//       <SplitPage Left={LeftComponent} Right={RightComponent} />
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+// * -- Layout Components V.2 -- 
+
+// import React from 'react'
+// import SplitPage from './components/splite-page'
+// import { RightComponent } from './components/right-component'
+// import { LeftComponent } from './components/left-component'
+
+// const App = () => {
+//   return (
+//     <>
+//       <SplitPage leftWidth={2} rightWidth={1} >
+//         <LeftComponent title={"hello i'm left v.2"} />
+//         <RightComponent title={"hello i'm rigt v.2"} />
+//       </SplitPage>
+//     </>
+//   )
+// }
+
+// export default App
