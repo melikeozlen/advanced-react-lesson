0f1ad8403bc8e6fb886d502a9de1e0e5bd74e892
melikeozlen <melikeozlen@gmail.com>
2023-12-09 20:42:16 +0300

    Checking Props with HOC comp.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
+import { logProps } from './components/HOC/log-props'
 import { UserInfo } from './components/LoaderComponent/user-info'
-import axios from 'axios'
-import DataSourceWithRender from './components/LoaderComponent/data-source-with-render'
-import DataSource from './components/LoaderComponent/data-source'
 
-const getDAtaFromServer = async (url) => {
-  const response = await axios.get(url);
-  return response.data
-}
-const getDataFromLocalStroge = (key) => () => {
-  return localStorage.getItem(key)
-}
-const Message = ({ msg }) => <h1>{msg}</h1>
+const UserInfoWarapper = logProps(UserInfo)
 const App = () => {
   return (
     <>
-      {/* <DataSourceWithRender getData={() => getDAtaFromServer(`http://localhost:9090/users/3`)} render={(resource) => <UserInfo user={resource} />} /> */}
-
-      <DataSource getData={() => getDataFromLocalStroge("key")} resourceName={"msg"}>
-        <Message />
-      </DataSource>
+      <UserInfoWarapper test={"test"} b="I'm be" c={21}/>
     </>
   )
 }
